960cad4c488fc326ae9a2ada44c16761d746d303
bsson2407 <bsson2407@gmail.com>
2022-11-11 12:53:12 +0700

    update file

--- src/component/chat/chat-main/fileMessage/fileMessage.component.tsx
+++ src/component/chat/chat-main/fileMessage/fileMessage.component.tsx
@@ -1,12 +1,76 @@
+import { Backdrop, Dialog, DialogContent, Modal } from '@material-ui/core';
+import { url } from 'inspector';
 import React, { useEffect, useState } from 'react';
+import BaseModal from '../../chat-header/call-modal/base-model.component';
 import './fileMessage.styles.scss';
 
 const FileMessage = ({ fileUrl, message, fileLink }: any) => {
   const [pathFile, setPathFile] = useState('');
+  const [viewFile, setViewFile] = useState(false);
+  const [docText, setDocText] = useState<any>(null);
   useEffect(() => {
     setPathFile(fileUrl.split('.')[fileUrl.split('.').length - 1]);
   }, []);
-  //   const pathFile = fileUrl.split('.')[fileUrl.split('.').length - 1];
+
+  const handleCloseModal = () => {
+    setViewFile(false);
+  };
+
+  const showViewFile = (file: any) => {
+    console.log(fetch(file));
+    fetch(file)
+      .then((response) => {
+        console.log(response);
+        return response.text();
+      })
+      .then((data) => {
+        console.log(data);
+        setDocText(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    // let read = fetch(file);
+    console.log(docText);
+    return (
+      <Modal
+        open={viewFile}
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          minHeight: '900px',
+          minWidth: '600px',
+        }}
+        onClose={handleCloseModal}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <iframe style={{ height: '900px', width: '700px' }} src={file}></iframe>
+      </Modal>
+      // <Dialog
+      //   open={viewFile}
+      //   onClose={handleCloseModal}
+      //   aria-labelledby="alert-dialog-title"
+      //   aria-describedby="alert-dialog-description"
+      // >
+      //   <DialogContent>
+      //     <form style={{ height: '900px', width: '700px' }}>
+      //       <div className="title">
+      //         <div className="close" onClick={() => handleCloseModal()}></div>
+      //       </div>
+      //       <iframe
+      //         style={{ height: '900px', width: '700px' }}
+      //         src={file}
+      //       ></iframe>
+      //     </form>
+      //   </DialogContent>
+      // </Dialog>
+    );
+  };
 
   return (
     <div className="message-file">
@@ -25,6 +89,14 @@ const FileMessage = ({ fileUrl, message, fileLink }: any) => {
       <a href={fileUrl} download>
         <i className="fal fa-download"></i>
       </a>
+      {pathFile === 'txt' && (
+        <>
+          <span onClick={() => setViewFile((e) => !e)}>
+            <i className="fal fa-eye"></i>
+          </span>
+          {viewFile && showViewFile(fileUrl)}
+        </>
+      )}
     </div>
   );
 };
